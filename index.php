32dc0f9157a76dc87464cdd4a70f4a62a6f7d4a0
Alonso <ramonalonso_giron@ucol.mx>
2021-01-07 20:01:21 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Animated-Navbar</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		
+		<nav>
+			<div class="hamburger">
+				<div class="line"></div>
+				<div class="line"></div>
+				<div class="line"></div>
+			</div>
+			<ul class="nav-links">
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Proyects</a></li>
+				<li><a href="#">Views</a></li>
+			</ul>
+		</nav>
+		
+		<section class="landing">
+			<h1>Hey Hello Everybody on Github</h1>
+		</section>
+		
+		
+		<script src="app.js"></script>
+	</body>
+</html>
